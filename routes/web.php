6ba09cdf30190f12b2bba57dbfc74cd9388d259c
Zteveluke <steveluke2606@gmail.com>
2021-03-17 15:33:54 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 Route::get('/search', function () {
     return view('search');
